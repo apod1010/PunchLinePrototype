d4eb663f8dc16779a6fb88981ef72ae4d75b284c
unknown <Anthony.Podraza@iam.local>
2018-02-15 15:15:43 -0600

    Initital Commit
    Camera ratio set to mobile
    basic model

---
+++ Assets/SwipeManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum SwipeDirection
+{
+    None = 0,
+    Left = 4,
+    Right = 6,
+    Up = 8,
+    Down = 2,
+
+    LeftDown = 1,
+    LeftUp = 7,
+    RightDown = 3,
+    RightUp = 9
+}
+
+public class SwipeManager : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
